e5bef1cb320533e6614b50163b639fda74e53bd5
YOUZYX <80650550+YOUZYX@users.noreply.github.com>
2025-05-29 17:52:40 +0000

    Manifestupdate

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,9 +4,9 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     "accountAssociation": {
-    "header": "eyJmaWQiOjEwNjE4OTgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhhMDQxYjE2OENFMjI4MDhBNDkwMWQ0NzdBYTkyMzg0OWQyMDUzYUUwIn0",
+    "header": "eyJmaWQiOjEwNjE4OTgsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgwQmYwOTBmN2ZBOWI2NTQyOTlBZGMzODA1Y2I5ODVlOTQ3QjNlNWM3In0",
     "payload": "eyJkb21haW4iOiJtb25henpsZS52ZXJjZWwuYXBwIn0",
-    "signature": "MHgxN2U3YWU5Y2FlNjY3YmY3YTg0NDBiNmU1NWIzYTU4MThhODE4MDhjNmRiMGE3YzFiMWM5MGZkNDJjOWU2MjMxNzU1NWRjYjI0MzA2ZDRhYjM5MDVlYTU4Y2NiNzA3NGJiMmViMGM5ZTE5YTJiNzVlNmRiZDkxNTNmMGYwYTY1NzFj"
+    "signature": "MHg0MmI4NzExMjdkMDE2MmRiZDRmNTE5YmJjMmY3NDczNDFkOTRjNDFmMzU5NDNiZjUzMzQyMWRhZmI0ZjdhYjhhMzQ0OTY1Mzk0ZDM3ZjlhN2JiMDdhZTYwNjA2ZTk2ZWEzYjM2ZmRkNzA2MDdmMzNhZWIzZGFlMTFjNjZmZjU5NzFi"
   },
     "frame": {
       "version": "1",
